949146e77b1273ffe9aaf59b54a20bb063e2f33e
hakim <hakim_yahi@hotmail.fr>
2024-02-10 16:52:31 +0100

    add react icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Users from './components/Users';
+import { SiAllocine } from "react-icons/si";
 
 function App() {
   return (
     <div className="App">
       <Users name="test"/>
       <Users/>
+      <h2> allons trouver un film sur <SiAllocine style={{ color: "orange" }}/></h2>
     </div>
   );
 }
